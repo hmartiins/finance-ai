620b46d3b3ab865096e9b6b711a318e9ecf21973
hmartiins <hmartins224@gmail.com>
2024-12-15 16:36:51 -0300

    feat: integrate Firebase with configuration options for Android and iOS

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,6 @@
 import 'package:finance_ai/config/dependencies.dart';
+import 'package:finance_ai/config/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:flutter/material.dart';
@@ -10,7 +12,13 @@ import 'main.dart';
 /// Launch with `flutter run --target lib/main_development.dart`.
 /// Uses local data.
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MultiProvider(
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,4 +1,6 @@
 import 'package:finance_ai/config/dependencies.dart';
+import 'package:finance_ai/config/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
@@ -9,7 +11,13 @@ import 'main.dart';
 /// Launch with `flutter run --target lib/main_staging.dart`.
 /// Uses remote data from a server.
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MultiProvider(
