5a9208f0c829029517a646769a1e89ff623aef89
hmartiins <hmartins224@gmail.com>
2024-12-21 11:02:33 -0300

    feat: add ExpenseCreateUseCase provider to dependency configuration

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -4,31 +4,37 @@ import 'package:finance_ai/adapters/storage/storage_adapter.dart';
 import 'package:finance_ai/data/repositories/expense/expense_repository.dart';
 import 'package:finance_ai/data/repositories/expense/expense_repository_remote.dart';
 import 'package:finance_ai/data/services/openai_service.dart';
+import 'package:finance_ai/domain/expanse/usecases/expense_create_use_case.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 /// Shared providers for all configurations.
 List<SingleChildWidget> _sharedProviders = [
   Provider(
-    create: (context) => OpenAIService(),
-  ),
-  Provider<IStorageAdapter>(
-    create: (context) => FirebaseAdapter(
-      firestore: FirebaseFirestore.instance,
+    create: (context) => ExpenseCreateUseCase(
+      expenseRepository: context.read(),
     ),
-  ),
+  )
 ];
 
 /// Configure dependencies for remote data.
 /// This dependency list uses repositories that connect to a remote server.
 List<SingleChildWidget> get providersRemote {
   return [
-    ..._sharedProviders,
+    Provider(
+      create: (context) => OpenAIService(),
+    ),
+    Provider<IStorageAdapter>(
+      create: (context) => FirebaseAdapter(
+        firestore: FirebaseFirestore.instance,
+      ),
+    ),
     Provider(
       create: (context) => ExpenseRepositoryRemote(
         storage: context.read<IStorageAdapter>(),
       ) as ExpenseRepository,
     ),
+    ..._sharedProviders,
   ];
 }
 
@@ -37,11 +43,19 @@ List<SingleChildWidget> get providersRemote {
 /// The user is always logged in.
 List<SingleChildWidget> get providersLocal {
   return [
-    ..._sharedProviders,
+    Provider(
+      create: (context) => OpenAIService(),
+    ),
+    Provider<IStorageAdapter>(
+      create: (context) => FirebaseAdapter(
+        firestore: FirebaseFirestore.instance,
+      ),
+    ),
     Provider(
       create: (context) => ExpenseRepositoryRemote(
         storage: context.read<IStorageAdapter>(),
       ) as ExpenseRepository,
     ),
+    ..._sharedProviders,
   ];
 }
